8d73b86c87e210fd664b75f663da3554004e69bd
pisces7diana <email7diana@gmail.com>
2023-12-16 21:07:09 -0800

    Setting up your database completed

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
+
+const drinks = require('./models/drinks.js');
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Gitpub App!")
+})
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
 })
 
 
