0681eec8dd15acfb061d968438c93abf97dde243
pisces7diana <email7diana@gmail.com>
2023-12-16 20:46:34 -0800

    Setting up your server completed

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('hi')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Gitpub App!")
+})
+
+
+
+
+
+
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`)
+})
